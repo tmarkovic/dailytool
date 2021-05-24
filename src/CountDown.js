e8f2f339134c4d12e38a69fb524501fc4d97ddc6
Theodore Markovic <theodore.markovic@svenskaspel.se>
2021-05-24 21:56:35 +0200

    #1 use craco for build script, fix lint warnings

--- src/CountDown.js
+++ src/CountDown.js
@@ -15,7 +15,7 @@ const CountDown = ({
     if (h === 0 && m === 0 && s === 0) setOver(true);
     else if (m === 0 && s === 0) {
       setTime([h - 1, 59, 59]);
-    } else if (s == 0) {
+    } else if (s === 0) {
       setTime([h, m - 1, 59]);
     } else {
       setTime([h, m, s - 1]);
